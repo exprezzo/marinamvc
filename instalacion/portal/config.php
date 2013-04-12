c0da9b9a32c6294dc1adc0c22ab45545fdd398e6
colab_01.diem <runtim3.error@gmail.com>
2013-04-12 11:45:50 -0600

    ...

---
+++ instalacion/portal/config.php
@@ -0,0 +1,15 @@
+<?php
+$DB_CONFIG=array(
+	'DB_SERVER'	=>'localhost',
+	'DB_NAME'	=>'app_db',
+	'DB_USER'	=>'root',
+	'DB_PASS'	=>'',
+	'PASS_AES'  =>'un4_c4d3n4_p*r4_encr!pt#r'
+);
+
+$APP_CONFIG=array(
+	'nombre'=>'Nombre de la aplicación',
+	'tema'=>'rocket'
+);
+
+?>
